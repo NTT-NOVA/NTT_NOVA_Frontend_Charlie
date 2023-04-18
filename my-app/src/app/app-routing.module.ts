de15fef0876d37eab649fae36988e2cbf5a26c50
Mikel <mklgsii@gmail.com>
2023-04-18 14:40:07 +0200

    task-edit added

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TaskComponent } from './task/task.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent},
   { path: 'new', component: TaskComponent},
+  { path: 'edit/:id', component: TaskEditComponent}
 ];
 
 @NgModule({
